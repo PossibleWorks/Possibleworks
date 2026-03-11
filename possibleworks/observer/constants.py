4160f77f718c4a98d685213959e9b47c716910e0
Yaswanth-Vempuluru-7916 <yaswanthvempuluru@outlook.com>
2026-03-11 10:32:50 +0530

    refactor: Remove unused constants and events from observer configuration

--- possibleworks/observer/constants.py
+++ possibleworks/observer/constants.py
@@ -1,42 +1,6 @@
 # Frappe/ERPNext system doctypes that should be ignored by observer
 # These are internal system events that create massive noise
 
-IGNORED_DOCTYPES = {
-    "Version",
-    "Comment",
-    "Workflow Action",
-    "Communication",
-    "Activity Log",
-    "Error Log",
-    "Document Share",
-    "Event",
-    "Task",
-    "ToDo",
-    "Notification",
-    "Notification Count",
-    "System Settings",
-    "Notification Log",
-    "Email Account",
-    "Email Queue",
-    "User",
-    "Role",
-    "Permission",
-    "Custom Field",
-    "Customize Form",
-    "DocType",
-    "DocField",
-    "DocPerm",
-}
-
-# Events to observe
-OBSERVED_EVENTS = [
-    "after_insert",
-    "on_update",
-    "on_submit",
-    "on_cancel",
-    "before_insert",
-    "before_update",
-]
 
 # Redis configuration
 REDIS_PREFIX = "possibleworks:event"
